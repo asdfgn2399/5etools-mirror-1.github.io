89e43caa66e1066b2f1a1d5d47dce2a08ff43a9b
asdfgn2399 <alexannett88@gmail.com>
2023-08-13 17:27:08 -0700

    1.183.1.5

--- .github/setEnvironment.sh
+++ .github/setEnvironment.sh
@@ -1,11 +1,2 @@
-#!/bin/bash
-
-set -e
-
-if [[ $# -eq 0 ]]; then
-    echo "No arguments provided. Usage: setEnvironment.sh"
-    exit 999
-fi
-
 mkdir env
 echo "export const environment = { firebaseApiKey : $1 }"  > env/env.js
